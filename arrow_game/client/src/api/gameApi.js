a67b0de435b403d1251152729c636bf2f80c3dc6
RaviMca <ravisalunkhe245@gmail.com>
2026-03-27 11:45:28 +0530

    deploy changes done

--- arrow_game/client/src/api/gameApi.js
+++ arrow_game/client/src/api/gameApi.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-
+import * as React from "react";
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: import.meta.env.VITE_API_URL
 });
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
 export const saveScore = async (score, level, token) => {
 
     return API.post(
